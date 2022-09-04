a0777475e70d7aedbf32d174dd8df70e3cfe4d1d
rchatt6 <rchatt6@uic.edu>
2022-09-04 17:10:22 -0500

    Finished 12hrTimeScript for KnickKnackMerge

--- Assets/setLocalTime12hr.cs
+++ Assets/setLocalTime12hr.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.Networking;
 using TMPro;
@@ -7,37 +9,32 @@ using TMPro;
 [System.Serializable]
 public class timeInfo
 {
-    string abb;
-    string client_ip;
-    string datetime;
-    int doyOfWeek;
-    int dayOfYear;
-    bool dst;
-    string dstFrom;
-    int dst_offset;
-    string dstUntil;
-    int rawOffset;
-    string timeZone;
-    long unixTime;
-    string utcDatetime;
-    string utcOffset;
-    int weekNumber;
-
-    public string getDateTime()
-    {
-        return datetime;
-    }
+    public string abbreviation;
+    public string client_ip;
+    public string datetime;
+    public int day_of_week;
+    public int day_of_year;
+    public bool dst;
+    public string dst_from;
+    public int dst_offset;
+    public string dst_until;
+    public int raw_offset;
+    public string timezone;
+    public int unixtime;
+    public string utc_datetime;
+    public string utc_offset;
+    public int week_number;
 
 }
 
-public class setLocalTime : MonoBehaviour
+public class setLocalTime12hr : MonoBehaviour
 {
     public GameObject timeTextObject;
     string url = "https://worldtimeapi.org/api/timezone/Europe/Paris";
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("GetDataFromWeb", 2f, 30f);
+        InvokeRepeating("GetDataFromWeb", 2f, 15f);
     }
 
     void GetDataFromWeb()
@@ -64,7 +61,12 @@ public class setLocalTime : MonoBehaviour
 
                 var json = webRequest.downloadHandler.text;
                 timeInfo data = JsonUtility.FromJson<timeInfo>(json);
-                Debug.Log(":\nTesting: " + data.getDateTime());
+                string timeSubstring = data.datetime.Substring(data.datetime.IndexOf("T") + 1, data.datetime.IndexOf(".")-11);
+                DateTime dt = DateTime.Parse(timeSubstring);
+                string time = dt.ToString("hh:mm tt");
+                Debug.Log(":\n12HourTime: " + time);
+
+                timeTextObject.GetComponent<TextMeshPro>().text = time;
             }
         }
     }
